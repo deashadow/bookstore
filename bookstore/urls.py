2738dfeddfbfc71f6554a8cd74065601be810507
deashadow <67285924+deashadow@users.noreply.github.com>
2020-10-29 15:35:19 -0400

    added an additional bstore.html file additional path in urls.py to include bstore

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
+    # Examples:
+    # path(r'^$', 'bookstore.views.home', name='home'),
+    # path(r'^blog/', include('blog.urls')),
+    path('', include('store.urls')),
     path('admin/', admin.site.urls),
 ]
--- store/views.py
+++ store/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+def template(request):
+    return render(request, 'template.html');
+
+def bstore(request):
+    return render(request, 'bstore.html');
