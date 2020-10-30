59d17dc3ef180422f4812dbd534bd0515083ad00
deashadow <67285924+deashadow@users.noreply.github.com>
2020-10-30 09:56:41 -0400

    added additional books, price and stock fields added new logic to include context and count to update bstore

--- store/views.py
+++ store/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 
+from .models import Book
+
 def template(request):
     return render(request, 'template.html');
 
 def bstore(request):
-    return render(request, 'bstore.html');
+    count = Book.objects.all().count()
+    context = {
+        'count': count,
+    }
+    return render(request, 'bstore.html', context);
